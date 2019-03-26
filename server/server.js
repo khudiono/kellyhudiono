bfa34def9cef5844d23b3894b4815b8c9e6cdf28
Kelly Hudiono <kellyhudiono@gmail.com>
2019-03-25 18:57:50 -0700

    change port number

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
