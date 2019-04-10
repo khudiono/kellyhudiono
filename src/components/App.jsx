679fcac7bd930fbd4f4d7067e2818c0a3a28a51d
Kelly Hudiono <kellyhudiono@gmail.com>
2019-04-10 11:27:07 -0700

    change nav bar + styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,29 @@ import Projects from './Projects';
 class App extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      nav: ''
+    }
+    this.scrollEvent = this.scrollEvent.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollEvent)
+  }
+
+  scrollEvent(e) {
+    if (window.scrollY > 300) {
+      this.setState({nav: 'nav-bar-color'});
+    }
+    if (window.scrollY < 300) {
+      this.setState({nav: ''});
+    }
   }
 
   render() {
     return(
       <div>
-        <div id="nav">
+        <div id="nav" className={this.state.nav}>
           <ul>
             <li><a href="#about"> About</a></li>
             <li><a href="#projects"> Projects</a></li>
