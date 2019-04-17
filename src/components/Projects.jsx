62036a7e4b0450790c2b18525c08afc7480a6cfe
Kelly Hudiono <kellyhudiono@gmail.com>
2019-04-16 17:53:31 -0700

    fix links on resume, make projects clickable

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,40 +6,42 @@ const Projects = () => {
     <div id="projects">
       <div className="projects-header">
         <h2>Projects</h2>
-        <div className="project-button"
-          data-aos="fade-up"
-          data-aos-delay="600"
-          data-aos-duration="2000">
-            <a href="https://github.com/khudiono"
-              target="_blank"
-              data-aos="fade-up"
-              data-aos-delay="600"
-              data-aos-duration="2000">
+        <a href="https://github.com/khudiono" target="_blank">
+          <div className="project-button"
+            data-aos="fade-up"
+            data-aos-delay="600"
+            data-aos-duration="2000">
               VIEW MORE PROJECTS
-            </a>
           </div>
+          </a>
         </div>
         <LazyLoad>
         <div className="project-display">
-          <img className="weather-demo"
-            src="./images/weather-app.gif"
+          <a href="https://github.com/khudiono/react-native-weather-app">
+            <img className="weather-demo"
+              src="./images/weather-app.gif"
+                data-aos="fade-up"
+                data-aos-delay="400"
+                data-aos-duration="2000"
+              />
+          </a>
+          <a href="https://github.com/khudiono/Boba-Buddies">
+            <img className="mac"
+              className="boba-display-1"
+              src="./images/boba-1.png"
+              data-aos="fade-up"
+              data-aos-delay="600"
+              data-aos-duration="2000"
+            />
+          </a>
+          <a href="https://github.com/khudiono/Boba-Buddies">
+            <img className="boba-display-2"
+              src="./images/boba-2.png"
               data-aos="fade-up"
               data-aos-delay="400"
               data-aos-duration="2000"
             />
-          <img className="mac"
-            className="boba-display-1"
-            src="./images/boba-1.png"
-            data-aos="fade-up"
-            data-aos-delay="600"
-            data-aos-duration="2000"
-          />
-          <img className="boba-display-2"
-            src="./images/boba-2.png"
-            data-aos="fade-up"
-            data-aos-delay="400"
-            data-aos-duration="2000"
-          />
+          </a>
         </div>
       </LazyLoad>
     </div>
